2394f761eacd9ee94b7eb3ce1fd35a376600e94c
jussimar <jussimar@jussimarleal.com.br>
2022-04-01 18:45:34 -0300

    aula 01/04 - criando componentes para o react native

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Componente from './components/Componente';
+import Tabuada from './components/Tabuada';
 
 export default function App() {
+  const [num, setNum] =  useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Componente nome="Maria" idade="40"/>
+
+      <View style={styles.row}>
+        <Text style={styles.texto}>Numero</Text>
+        <TextInput 
+          style={styles.input}
+          value={num}
+          onChangeText={(value)=>setNum(value)}
+        />
+      </View>
+      <Tabuada num={num}/>
     </View>
   );
 }
@@ -17,4 +29,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row:{
+    width:'100%',
+    marginTop:10
+  },
+  input:{
+    width:'80%',
+    borderWidth:1,
+    marginLeft:'10%',
+    fontSize:30,
+    borderRadius:6
+  },
+  texto:{
+    fontSize:30,
+    marginLeft:'10%'
+  }
 });
